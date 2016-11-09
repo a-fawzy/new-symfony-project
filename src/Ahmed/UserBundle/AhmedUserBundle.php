85b111386884642b43492dad125ca644c8c47e2d
ahmed <ahmed@ahmed-LIFEBOOK-AH532-G52>
2016-11-09 14:14:39 +0200

    modify the default login page view

--- src/Ahmed/UserBundle/AhmedUserBundle.php
+++ src/Ahmed/UserBundle/AhmedUserBundle.php
@@ -4,6 +4,10 @@ namespace Ahmed\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AhmedUserBundle extends Bundle
-{
+class AhmedUserBundle extends Bundle {
+
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
+
 }
